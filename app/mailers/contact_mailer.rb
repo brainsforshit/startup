7736eb653e8c628030a49b0c0470b42308dc82f8
Boris Callebert <callebert.boris@outlook.be>
2016-07-14 12:13:12 +0000

    Added mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,6 @@
 class ContactMailer < ActionMailer::Base
-    default to:'callebert.boris@outlook.be'
+    default to: 'callebert.boris@outlook.be'
+    
     def contact_email(name,email,body)
         @name = name
         @email = email
